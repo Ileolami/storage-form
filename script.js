b46c1a700bc1fdda55ca3e5af9192695d85f77e3
ileolami <makindemercymiracle@gmail.com>
2023-09-17 20:05:17 +0100

    update

--- script.js
+++ script.js
@@ -38,28 +38,6 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             alert('Please fill in all fields!');
         }
+        formLogin.reset();
     });
 });
-
-
-// const formLogin = document.querySelector('#login');
-// formLogin.addEventListener('DOMContentLoaded', function(e) {
-//     e.preventDefault();
-
-//     const loginUsername = document.querySelector('#login-username').value;
-//     const loginPassword = document.querySelector('#login-password').value;
-
-//     if (loginUsername && loginPassword) {
-//         const user = users.find(u => u.username === loginUsername && u.password === loginPassword);
-//         if (user) {
-//             alert('Login successful!');
-//             // Redirect to another page or perform necessary actions after login
-//             // For example, window.location.href = 'index.html';
-//             window.location.href = 'index.html';
-//         } else {
-//             alert('Username or password is incorrect!');
-//         }
-//     } else {
-//         alert('Please fill in all fields!');
-//     }
-// });
